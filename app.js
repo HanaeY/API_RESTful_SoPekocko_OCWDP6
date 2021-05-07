96d9ee0a6e1f942a38239cd15c8d0263535ce3fa
Hanae <hanae.yamashita@lilo.org>
2021-05-07 10:57:27 +0200

    configuration base de données

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 // importer express
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://SuperAdmin:fONRBkxvs5v7CV1v@cluster0.blsha.mongodb.net/SoPekockoDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
     console.log('requête reçue !');

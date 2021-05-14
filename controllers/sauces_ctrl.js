0d6c06c7af64e20cee2534978ce7307e62cdf63b
Hanae <hanae.yamashita@lilo.org>
2021-05-14 13:23:16 +0200

    controlleurs routes like/dislike

--- controllers/sauces_ctrl.js
+++ controllers/sauces_ctrl.js
@@ -71,3 +71,82 @@ exports.getOneSauce = (req, res, next) => {
         .then(sauce => res.status(200).json(sauce))
         .catch(error => res.status(404).json({error}));
 };
+
+exports.likeASauce = (req, res, next) => {
+    const userId = req.body.userId;
+    Sauce.findOne({_id: req.params.id})
+        .then(sauce => {
+            switch(req.body.like) {
+                case 1 : 
+                    if(sauce.userLiked.includes(userId)) {
+                        //res.status(200).json({message: 'sauce déjà aimée par cet utilisateur !'});
+                        removeLike();
+                    } else {
+                        sauce.userLiked.push(userId);
+                        sauce.likes ++;
+                        sauce._id = req.params.id;
+                        Sauce.updateOne({_id: req.params.id}, sauce)
+                            .then(() => res.status(200).json({message : "J'aime ajouté !"}))
+                            .catch(error => res.status(400).json({error}));
+                    }
+                    break; 
+                case -1 :
+                    if(sauce.userDisliked.includes(userId)) {
+                        res.status(200).json({message: 'sauce déjà desaimée par cet utilisateur !'});
+                    } else {
+                        sauce.userDisliked.push(userId);
+                        sauce.dislikes ++;
+                        sauce._id = req.params.id;
+                        Sauce.updateOne({_id: req.params.id}, sauce)
+                            .then(() => res.status(200).json({message : "je n'aime pas ajouté !"}))
+                            .catch(error => res.status(400).json({error}));
+                    }
+                break;
+                case 0 : 
+                const userLikedIndex = sauce.userLiked.indexOf(userId);
+                const userDislikedIndex = sauce.userDisliked.indexOf(userId);
+                    if(userLikedIndex !== -1) {
+                        sauce.userLiked.splice(userLikedIndex, 1);
+                        sauce.likes --;
+                        sauce._id = req.params.id;
+                        Sauce.updateOne({_id: req.params.id}, sauce)
+                        .then(() => res.status(200).json({message : "j'aime supprimé !"}))
+                        .catch(error => res.status(400).json({error}));
+                    }
+                    if(userDislikedIndex !== -1) {
+                        sauce.userDisliked.splice(userDislikedIndex, 1);
+                        sauce.dislikes --;
+                        sauce._id = req.params.id;
+                        Sauce.updateOne({_id: req.params.id}, sauce)
+                        .then(() => res.status(200).json({message : "je n'aime pas supprimé !"}))
+                        .catch(error => res.status(400).json({error}));
+                    }
+                    break;       
+                default : 
+                throw 'aucune opération possible !';            
+            };
+        })
+        .catch(error => {res.status(500).json({error})});
+};
+
+const removeLike = () => {
+    const userLikedIndex = sauce.userLiked.indexOf(userId);
+    sauce.userLiked.splice(userLikedIndex, 1);
+    sauce.likes --;
+    sauce._id = req.params.id;
+    Sauce.updateOne({_id: req.params.id}, sauce)
+    .then(() => res.status(200).json({message : "j'aime supprimé !"}))
+    .catch(error => res.status(400).json({error}));
+};
+
+// retrouver la sauce en question 
+// si like = 1, regarder si l'utilisateur n'est pas déjà dans le tableau des userLiked
+    // si oui, erreur
+    // si non, ajouter l'id user dans le tableau des userLiked, ET ajouter incrémenter de 1 le nombre de likes
+// si like = -1, regarder sil'utilisateur n'est pas déjà dans le tableau des userDisliked
+    // si oui, erreur
+    // si non, ajouter l'id user dans le tableau des userDisliked, ET incrémenter les dislikes de 1
+// si like = O, regarder si l'utilisateur est dans le tableau des userLiked
+    // si oui, retirer l'id user du tableau ET décrémenter les likes de 1
+    // si non, regarder si d'id user est dans le tableau des userDisliked
+        // si oui, retirer l'id user du tableau ET décrémenter les dislikes de 1

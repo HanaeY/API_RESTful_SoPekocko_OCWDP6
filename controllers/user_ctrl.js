52d28742bd84290c206d1f8b17de1ff18469db99
Hanae <hanae.yamashita@lilo.org>
2021-05-20 14:57:27 +0200

    correction authentification

--- controllers/user_ctrl.js
+++ controllers/user_ctrl.js
@@ -32,7 +32,7 @@ exports.login = (req, res, next) => {
                             userId : user._id,
                             token : jwt.sign(
                                 {userId : user._id}, // le payload qui sera intégré au token
-                                'process.env.SECRET_KEY', 
+                                process.env.SECRET_KEY, 
                                 {expiresIn: '24h'}
                             )
                         });
